80be9b461a4e585504f66bb09b8077e586ff65ce
Salvatore Buokaleb <salvatore@easystaff.it>
2021-10-24 15:18:40 +0200

    ci fix

--- auth/src/controllers/__test__/UserController@currentUser.test.ts
+++ auth/src/controllers/__test__/UserController@currentUser.test.ts
@@ -2,16 +2,22 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with details about the current user", async () => {
-  await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
+  const cookie = await global.signin();
   const res = await request(app)
     .get("/api/users/currentuser")
+    .set("Cookie", cookie)
     .send()
     .expect(200);
-  expect(res.body.user).toBeDefined();
+  expect(res.body.currentUser).toBeDefined();
+  expect(res.body.currentUser.email).toBe("test@test.com");
+});
+
+it("responds with null if not authenitcated", async () => {
+  const res = await request(app)
+    .get("/api/users/currentuser")
+
+    .send()
+    .expect(200);
+
+  expect(res.body.currentUser).toEqual(null);
 });
