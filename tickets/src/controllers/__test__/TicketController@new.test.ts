0fbc44d8fe0adbbd4fdf35ff68456f58aafdc2a9
Salvatore Buokaleb <salvatore@easystaff.it>
2021-10-30 14:45:31 +0200

    empty test case for new ticket route

---
+++ tickets/src/controllers/__test__/TicketController@new.test.ts
@@ -0,0 +1,9 @@
+import request from "supertest";
+import { app } from "../../app";
+
+
+it('has a route handler listening to /api/tickets from post request', async() => {});
+it('can only be accessed if user is signe in', async() => {});
+it('return an error if inavalid title is provided', async() => {});
+it('return an error if inavalid price is provided', async() => {});
+it('creates a ticket with valid inputs', async() => {});
